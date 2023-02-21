0650fc148d4344e15e158055c42608f5e15fbbc4
Joelyton Neto <joelyton.neto@atlantatecnologia.com.br>
2023-02-21 11:22:04 -0300

    cópia do módulo ecommerce para transformar em cadastro de perfil de acesso.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -72,6 +72,7 @@ export const appRoutes: Route[] = [
         },
         children: [
             {path: 'example', loadChildren: () => import('app/modules/admin/example/example.module').then(m => m.ExampleModule)},
+            {path: 'perfil', loadChildren: () => import('app/modules/admin/ecommerce/ecommerce.module').then(m => m.ECommerceModule)},
         ]
     }
 ];
